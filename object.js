054eef1b6b17e133372f052cd5905e69c94e09b9
SnowdenxEdward <snowdenxedward@gmail.com>
2024-10-01 16:23:03 +0530

    object.js

---
+++ object.js
@@ -0,0 +1,10 @@
+// Java script Objects
+const student = {
+    name : 'Edward Snowden',
+    college : 'Harvard University ',
+    course : 'computer science',
+    address : 'Massachusetts Hall, Cambridge, MA 02138, United States',
+};
+
+console.log(`${student.name}, ${student.college}, ${student.course}, ${student.address}`);
+
