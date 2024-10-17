dfa157196f0608736a9d0f46817049a537a18a1d
Edward <snowdenxedward@gmail.com>
2024-10-17 23:03:07 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,21 @@
+const accountId = 144567
+let accountEmail = "snowdengoogle.com"
+var accountPassword = "12345"
+accountCity = "san jose"
+let accountState;
+
+// 
+
+accountEmail = "snowden.com"
+accountPassword = "090909"
+accountCity = "new york"
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
