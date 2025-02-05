be3be46323c007f5cfbf569d07179df5a8c7b68e
Edward <snowdenxedward@gmail.com>
2025-02-05 21:38:39 +0530

    Create weatherAPI.js

---
+++ weatherAPI.js
@@ -0,0 +1,20 @@
+const button = document.getElementById('click')
+const userInput = document.getElementById('userInput')
+const outputCountry = document.getElementById('outputCountry')
+const temp = document.getElementById('temp')
+const time = document.getElementById('time')
+
+document.body.style.backgroundColor = 'black'
+document.body.style.color = 'white'
+
+button.addEventListener('click',()=>{
+    const userIn = userInput.value 
+  fetch(`http://api.weatherapi.com/v1/current.json?key=159d9ac7c6ba4ef4903150342250502&q=${userIn}&aqi=yes`)
+  .then(response => response.json())
+  .then(data => {
+    outputCountry.innerHTML=data.location.region
+    temp.innerHTML=data.current.temp_c
+    time.innerHTML=data.location.localtime
+    console.log(data)
+  })  
+})
