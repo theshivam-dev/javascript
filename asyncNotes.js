d113c735c0a61398684320555627f9c1bcdc72f8
Edward <snowdenxedward@gmail.com>
2025-02-12 15:21:07 +0530

    Create asyncNotes.js

---
+++ asyncNotes.js
@@ -0,0 +1,81 @@
+// ------------------------------------------------------------Promise---------------------------------------------------------------
+const promise = new Promise((resolve, reject) => {
+  const data = true;
+  if (data) {
+    resolve("resolved");
+  } else {
+    reject("its reject");
+  }
+});
+promise
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// ------------------------------------------------------------Fetch API---------------------------------------------------------------
+fetch("https://api.github.com/users/torvalds")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.location);
+  })
+  .catch((err) => {
+    console.log("this is a error ", err);
+  });
+// ------------------------------------------------------------CallbackHell---------------------------------------------------------------
+const wifiComplain = (callback) => {
+  setTimeout(() => {
+    console.log("complaint register");
+    callback();
+  }, 1000);
+};
+const complainAssign = (callback) => {
+  setTimeout(() => {
+    console.log("complaint assign to ISP");
+    callback();
+  }, 3000);
+};
+const complainProcess = (callback) => {
+  setTimeout(() => {
+    console.log("complain in processing");
+    callback();
+  }, 4000);
+};
+const complainResolved = (callback) => {
+  setTimeout(() => {
+    console.log("complain resolved");
+    callback();
+  }, 4000);
+};
+const complainClosed = (callback) => {
+  setTimeout(() => {
+    console.log("complain is going to closed");
+    callback();
+  }, 3000);
+};
+const customerFeedback = (callback) => {
+  setTimeout(() => {
+    console.log("gave feedback about our organization");
+  }, 1000);
+  callback();
+};
+
+wifiComplain(() => {
+  complainAssign(() => {
+    complainProcess(() => {
+      complainResolved(() => {
+        complainClosed(() => {
+          customerFeedback(() => {});
+        });
+      });
+    });
+  });
+});
+// ------------------------------------------------------------Aync Await---------------------------------------------------------------
+const asyncFetch = async (url) =>{
+const response = await (await fetch(url)).json()
+console.log(response);
+}
+
+asyncFetch('https://api.github.com/users/snowdenxedward') 
