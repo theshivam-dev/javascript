3cf32e73a94b525c620324d5060546ebb1e2bcfc
Edward <snowdenxedward@gmail.com>
2025-02-15 00:34:11 +0530

    Update asyncNotes.js

--- asyncNotes.js
+++ asyncNotes.js
@@ -100,3 +100,14 @@ async function asyncFetchData() {
   console.log(data);
 }
 asyncFetchData();
+// ------------------------------------------------------------Try Catch----------------------------------------------------------------
+const tryCatch = async () => {
+  try {
+    const response = await fetch('https://api.github.com/users/snowdenxedward');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+tryCatch();
